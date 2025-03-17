8f25292a3349430001c19a5e8504013e8cfe2597
sultan <zyldyzbekovsultan02@gmail.com>
2025-03-17 18:24:01 +0600

    На этом этапе реализовал только сложение. Пользователь вводит выражение, программа выполняет операцию и выводит результат.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+
+        double a = scanner.nextDouble();
+        char op = scanner.next().charAt(0);
+        double b = scanner.nextDouble();
+
+        double result = 0;
+
+        if (op == '+') {
+            result = a + b;
+        }
+        System.out.println("Результат: " + result);
+        scanner.close();
     }
 }
